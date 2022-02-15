c430fbf2598d7784a83727f0ca1b0dc4938d84fd
Smyja <akposlive59@gmail.com>
2022-02-15 10:15:32 +0100

    Fixed import issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
 import React from "react";
-import {BrowserRouter, Routes, Route } from 'react-router-dom';
-import About from "./components/About";
-import Home from "./components/Home";
-import Contact from "./components/Contact";
 import Layout from "./containers/Layout";
 
 function App() {
